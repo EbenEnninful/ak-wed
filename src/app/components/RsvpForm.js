c8f1727c224d554650530d8e9353fce65ebf3249
EbenEnninful <ebenneutron8@gmail.comgit config --global user.name EbenEnninfulgit config --global user.email ebenneutron8@gmail.com>
2025-07-04 23:42:20 +0000

    Updated the RSVP form

--- src/app/components/RsvpForm.js
+++ src/app/components/RsvpForm.js
@@ -75,7 +75,7 @@ export default function RsvpForm() {
                 <div>
                     <label className="block text-sm font-medium text-gray-700 mb-2">Which group are you with?</label>
                     <div className="flex items-center space-x-6">
-                        {['EWC', 'UCC', 'GWL'].map((groupName) => (
+                        {['EWC', 'UCC', 'GWL', 'OTHERS'].map((groupName) => (
                             <label key={groupName} className="inline-flex items-center">
                                 <input type="radio" name="group" value={groupName} checked={group === groupName} onChange={(e) => setGroup(e.target.value)} className="form-radio h-4 w-4 text-pink-600" />
                                 <span className="ml-2 text-gray-700">{groupName}</span>
