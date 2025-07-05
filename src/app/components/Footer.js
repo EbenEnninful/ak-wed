f9c011d075c5261d2232d5bf0bf5378393365c58
EbenEnninful <ebenneutron8@gmail.comgit config --global user.name EbenEnninfulgit config --global user.email ebenneutron8@gmail.com>
2025-07-05 00:13:09 +0000

    Updated the Footer

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default function Footer() {
     return (
         <footer className="text-center mt-12 pt-8 border-t border-gray-200 text-gray-500 text-sm">
-            <p>© {new Date().getFullYear()} ALMOST STUDIO. All Rights Reserved.</p>
+            <p>© {new Date().getFullYear()} ALMOST STUDIO.Made with ❤️ for Our Big Day </p>
         </footer>
     );
 }
+
