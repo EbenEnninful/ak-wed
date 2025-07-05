77c059416ac042527a5edddd7c60b5566cd80264
EbenEnninful <ebenneutron8@gmail.comgit config --global user.name EbenEnninfulgit config --global user.email ebenneutron8@gmail.com>
2025-07-05 00:21:29 +0000

    Updated the Map

--- src/app/components/Map.js
+++ src/app/components/Map.js
@@ -7,7 +7,7 @@ export default function Map() {
             <p className="text-gray-700">Find us at Prampram, Lower town.</p>
             <div className="relative overflow-hidden rounded-lg shadow-lg" style={{ paddingTop: '56.25%' }}>
                 <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.057199451125!2d-0.09192488522886738!3d5.554724995964795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf8b8b8b8b8b8b%3A0x8b8b8b8b8b8b8b8b!2sPrampram!5e0!3m2!1sen!2sgh!4v1678886400000!5m2!1sen!2sgh"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3970.022867897101!2d0.11129067591828018!3d5.709834394272086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x10207d7524a7a6e9%3A0x524ed40ec851262d!2sWest%20Africa%20Fishery!5e0!3m2!1sen!2sgh!4v1751674710527!5m2!1sen!2sgh"
                     width="100%"
                     height="100%"
                     style={{ border: 0, position: 'absolute', top: 0, left: 0 }}
@@ -20,3 +20,9 @@ export default function Map() {
         </div>
     );
 }
+
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3970.022867897101!2d0.11129067591828018!3d5.709834394272086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x10207d7524a7a6e9%3A0x524ed40ec851262d!2sWest%20Africa%20Fishery!5e0!3m2!1sen!2sgh!4v1751674710527!5m2!1sen!2sgh"
+ width="600"
+ height="450" style="border:0;" allowfullscreen=""
+ loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
