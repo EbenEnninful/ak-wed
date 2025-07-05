217323a86c899072b0f6f8d1279ce553f4d05bde
EbenEnninful <ebenneutron8@gmail.comgit config --global user.name EbenEnninfulgit config --global user.email ebenneutron8@gmail.com>
2025-07-05 05:58:29 +0000

    Final UI fixes and updates

--- src/app/components/Map.js
+++ src/app/components/Map.js
@@ -20,9 +20,3 @@ export default function Map() {
         </div>
     );
 }
-
-
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3970.022867897101!2d0.11129067591828018!3d5.709834394272086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x10207d7524a7a6e9%3A0x524ed40ec851262d!2sWest%20Africa%20Fishery!5e0!3m2!1sen!2sgh!4v1751674710527!5m2!1sen!2sgh"
- width="600"
- height="450" style="border:0;" allowfullscreen=""
- loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
