3632bc6ea83c00f207da0f864a0bae69f46e9893
Eben <ebenneutron8@gmail.com>
2025-08-01 18:00:23 +0000

    Fix: ESLint errors in RsvpForm and Gallery

--- src/app/components/RsvpForm.js
+++ src/app/components/RsvpForm.js
@@ -6,7 +6,7 @@ export default function RsvpForm() {
     return (
         <div className="text-center p-8 bg-white rounded-lg shadow-xl">
             <h2 className="text-2xl font-bold mb-4 text-gray-800">RSVP Is Now Closed</h2>
-            <p className="text-gray-600">Thank you to everyone who submitted their RSVP. We can't wait to celebrate with you!</p>
+            <p className="text-gray-600">Thank you to everyone who submitted their RSVP. We can\'t wait to celebrate with you!</p>
         </div>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
                 <Countdown weddingDate={weddingDate} />
 
                 
-                <RsvpForm />
+               
 
 
 
